1a5e0e567cdbed3e6ced4431c73fc18d35cf24dd
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-10-07 02:06:00 +0600

    properly formatted code

--- Color_Palette_Maker/main.py
+++ Color_Palette_Maker/main.py
@@ -1,23 +1,24 @@
 import tkinter
 from tkinter import BOTH, IntVar, DISABLED, filedialog
 
-#Define window
+# Define window
 root = tkinter.Tk()
 root.title('Color Theme Maker')
 root.geometry('550x600')
-root.resizable(0,0)
+root.resizable(0, 0)
 
-#Define functions
+
+# Define functions
 def get_red(slider_value):
     """Turn current slider value for red into a hex value and update color.
     The scale value is passed automatically when the scale is moved calling the get_red function."""
     global red_value
 
-    #Turn the slider value into an int and hex value. Strip leading chars so only two remain
+    # Turn the slider value into an int and hex value. Strip leading chars so only two remain
     red_value = hex(int(slider_value))
     red_value = red_value.lstrip("0x")
 
-    #If hex value is single digit, lead with a 0 such that d becomes 0d
+    # If hex value is single digit, lead with a 0 such that d becomes 0d
     while len(red_value) < 2:
         red_value = "0" + str(red_value)
 
@@ -29,14 +30,14 @@ def get_green(slider_value):
     The scale value is passed automatically when the scale is moved calling the get_green function."""
     global green_value
 
-    #Turn the slider value into an int and hex value. Strip leading chars so only two remain
+    # Turn the slider value into an int and hex value. Strip leading chars so only two remain
     green_value = hex(int(slider_value))
     green_value = green_value.lstrip("0x")
 
-    #If hex value is single digit, lead with a 0 such that d becomes 0d
+    # If hex value is single digit, lead with a 0 such that d becomes 0d
     while len(green_value) < 2:
         green_value = "0" + str(green_value)
-        
+
     update_color()
 
 
@@ -45,29 +46,30 @@ def get_blue(slider_value):
     The scale value is passed automatically when the scale is moved calling the get_blue function."""
     global blue_value
 
-    #Turn the slider value into an int and hex value. Strip leading chars so only two remain
+    # Turn the slider value into an int and hex value. Strip leading chars so only two remain
     blue_value = hex(int(slider_value))
     blue_value = blue_value.lstrip("0x")
 
-    #If hex value is single digit, lead with a 0 such that d becomes 0d
+    # If hex value is single digit, lead with a 0 such that d becomes 0d
     while len(blue_value) < 2:
         blue_value = "0" + str(blue_value)
-        
+
     update_color()
 
 
 def update_color():
     """UPdate the current color box based on the slider values.  Display tuple and hex values of the current color"""
-    #Make the color box smaller than the original due to ipadx and ipday on the original color box
+    # Make the color box smaller than the original due to ipadx and ipday on the original color box
     color_box = tkinter.Label(input_frame, bg="#" + red_value + green_value + blue_value, height=6, width=15)
     color_box.grid(row=1, column=3, columnspan=2, padx=35, pady=10)
 
-    #Display the tuple and hex value for the given color
-    color_tuple.config(text='(' + str(red_slider.get()) + '),' + '(' + str(green_slider.get()) + '),' + '(' + str(blue_slider.get()) + ')')
+    # Display the tuple and hex value for the given color
+    color_tuple.config(text='(' + str(red_slider.get()) + '),' + '(' + str(green_slider.get()) + '),' + '(' + str(
+        blue_slider.get()) + ')')
     color_hex.config(text="#" + red_value + green_value + blue_value)
 
 
-def set_color(r,g,b):
+def set_color(r, g, b):
     """Set a given color"""
     red_slider.set(r)
     green_slider.set(g)
@@ -78,11 +80,11 @@ def store_color():
     """Store the current color tuple value and display color"""
     global stored_colors
 
-    #Get the current value of each slider and append 0's to keep formatting
+    # Get the current value of each slider and append 0's to keep formatting
     red = str(red_slider.get())
     while len(red) < 3:
         red = "0" + red
-    
+
     green = str(green_slider.get())
     while len(green) < 3:
         green = "0" + green
@@ -91,74 +93,76 @@ def store_color():
     while len(blue) < 3:
         blue = "0" + blue
 
-    #Keep a reference of the current color
+    # Keep a reference of the current color
     stored_red = red_slider.get()
     stored_green = green_slider.get()
     stored_blue = blue_slider.get()
 
-    #Create new widgets for the stored color.
-    recall_button = tkinter.Button(output_frame, text="Recall Color", command=lambda:set_color(stored_red, stored_green, stored_blue))
-    new_color_tuple = tkinter.Label(output_frame, text='(' + red + '),' + '(' + green + '),' + '(' + blue + ')') 
+    # Create new widgets for the stored color.
+    recall_button = tkinter.Button(output_frame, text="Recall Color",
+                                   command=lambda: set_color(stored_red, stored_green, stored_blue))
+    new_color_tuple = tkinter.Label(output_frame, text='(' + red + '),' + '(' + green + '),' + '(' + blue + ')')
     new_color_hex = tkinter.Label(output_frame, text='#' + red_value + green_value + blue_value)
     new_color_black_box = tkinter.Label(output_frame, bg='black', width=3, height=1)
-    new_color_box = tkinter.Label(output_frame, bg="#"+ red_value + green_value + blue_value, width=3, height=1) 
+    new_color_box = tkinter.Label(output_frame, bg="#" + red_value + green_value + blue_value, width=3, height=1)
 
-    #Put new widgets on the screen
+    # Put new widgets on the screen
     recall_button.grid(row=stored_color.get(), column=1, padx=20)
     new_color_tuple.grid(row=stored_color.get(), column=2, padx=20)
     new_color_hex.grid(row=stored_color.get(), column=3, padx=20)
     new_color_black_box.grid(row=stored_color.get(), column=4, pady=2, ipadx=5, ipady=5)
     new_color_box.grid(row=stored_color.get(), column=4)
 
-    #Update the dict stored_colors with the new color tuple and hex values
+    # Update the dict stored_colors with the new color tuple and hex values
     stored_colors[stored_color.get()] = [new_color_tuple.cget("text"), new_color_hex.cget("text")]
 
-    #Move the radio button stored colors_ to the next value if available
+    # Move the radio button stored colors_ to the next value if available
     if stored_color.get() < 5:
         stored_color.set(stored_color.get() + 1)
 
 
 def save_colors():
     """Output the chosen colors to a txt file."""
-    #Get the directory where the user would like to save
-    file_name = filedialog.asksaveasfilename(initialdir='./', title='Save Colors', filetypes=(('Text', '.txt'),('All Files', '*.*')))
+    # Get the directory where the user would like to save
+    file_name = filedialog.asksaveasfilename(initialdir='./', title='Save Colors',
+                                             filetypes=(('Text', '.txt'), ('All Files', '*.*')))
 
-    #open the new file as write
+    # open the new file as write
     with open(file_name, "w") as f:
         f.write("Color Theme Maker Output\n")
         for saved_entry in stored_colors.values():
             f.write(saved_entry[0] + "\n" + saved_entry[1] + "\n\n")
-    
 
-#Define Layout
+
+# Define Layout
 input_frame = tkinter.LabelFrame(root, padx=5, pady=5)
 output_frame = tkinter.LabelFrame(root, padx=5, pady=5)
 input_frame.pack(fill=BOTH, expand=True, padx=5, pady=5)
 output_frame.pack(fill=BOTH, expand=True, padx=5, pady=5)
 
-#Setting up the input frame.
-#Create the labels, sliders, and buttons for each color RGB
+# Setting up the input frame.
+# Create the labels, sliders, and buttons for each color RGB
 red_label = tkinter.Label(input_frame, text="R")
 red_slider = tkinter.Scale(input_frame, from_=0, to=255, command=get_red)
-red_button = tkinter.Button(input_frame, text="Red", command=lambda:set_color(255,0,0))
+red_button = tkinter.Button(input_frame, text="Red", command=lambda: set_color(255, 0, 0))
 green_label = tkinter.Label(input_frame, text="G")
 green_slider = tkinter.Scale(input_frame, from_=0, to=255, command=get_green)
-green_button = tkinter.Button(input_frame, text="Green", command=lambda:set_color(0,255,0))
+green_button = tkinter.Button(input_frame, text="Green", command=lambda: set_color(0, 255, 0))
 blue_label = tkinter.Label(input_frame, text="B")
 blue_slider = tkinter.Scale(input_frame, from_=0, to=255, command=get_blue)
-blue_button = tkinter.Button(input_frame, text="Blue", command=lambda:set_color(0,0,255))
+blue_button = tkinter.Button(input_frame, text="Blue", command=lambda: set_color(0, 0, 255))
 
-#Create buttons for each complimentary color
-yellow_button = tkinter.Button(input_frame, text="Yellow", command=lambda:set_color(255,255,0))
-cyan_button = tkinter.Button(input_frame, text="Cyan", command=lambda:set_color(0,255,255))
-magenta_button = tkinter.Button(input_frame, text="Magenta", command=lambda:set_color(255,0,255))
+# Create buttons for each complimentary color
+yellow_button = tkinter.Button(input_frame, text="Yellow", command=lambda: set_color(255, 255, 0))
+cyan_button = tkinter.Button(input_frame, text="Cyan", command=lambda: set_color(0, 255, 255))
+magenta_button = tkinter.Button(input_frame, text="Magenta", command=lambda: set_color(255, 0, 255))
 
-#Create utility buttons
+# Create utility buttons
 store_button = tkinter.Button(input_frame, text="Store Color", command=store_color)
 save_button = tkinter.Button(input_frame, text="Save", command=save_colors)
 quit_button = tkinter.Button(input_frame, text="Quit", command=root.destroy)
 
-#Put labels, sliders, and buttons on to the frame....Use ipadx with rbg buttons to define column width, then use sticky on others
+# Put labels, sliders, and buttons on to the frame....Use ipadx with rbg buttons to define column width, then use sticky on others
 red_label.grid(row=0, column=0, sticky='W')
 red_slider.grid(row=1, column=0, sticky='W')
 red_button.grid(row=2, column=0, padx=1, pady=1, ipadx=20)
@@ -175,22 +179,22 @@ store_button.grid(row=4, column=0, columnspan=3, padx=1, pady=1, sticky="WE")
 save_button.grid(row=4, column=3, padx=1, pady=1, sticky="WE")
 quit_button.grid(row=4, column=4, padx=1, pady=1, sticky="WE")
 
-#Create the color box and color labels
+# Create the color box and color labels
 color_box = tkinter.Label(input_frame, bg='black', height=6, width=15)
 color_tuple = tkinter.Label(input_frame, text='(0), (0), (0)')
 color_hex = tkinter.Label(input_frame, text='#000000')
 
-#Put the color box and labels on the frame. 
+# Put the color box and labels on the frame.
 color_box.grid(row=1, column=3, columnspan=2, padx=35, pady=10, ipadx=10, ipady=10)
 color_tuple.grid(row=2, column=3, columnspan=2)
 color_hex.grid(row=3, column=3, columnspan=2)
 
-#Setting up the output frame
-#Initialize a dictionary to hold all stored colors
+# Setting up the output frame
+# Initialize a dictionary to hold all stored colors
 stored_colors = {}
 stored_color = IntVar()
 
-#Create radio buttons to select stored colors and populate each row with placeholder values
+# Create radio buttons to select stored colors and populate each row with placeholder values
 for i in range(6):
     radio = tkinter.Radiobutton(output_frame, variable=stored_color, value=i)
     radio.grid(row=i, column=0, sticky='W')
@@ -207,13 +211,13 @@ for i in range(6):
     new_color_black_box.grid(row=i, column=4, pady=2, ipadx=5, ipady=5)
     new_color_box.grid(row=i, column=4)
 
-    #.cget() returns the value of a specific option.  Store the text value of the tuple label and hex label
+    # .cget() returns the value of a specific option.  Store the text value of the tuple label and hex label
     stored_colors[stored_color.get()] = [new_color_tuple.cget('text'), new_color_hex.cget('text')]
 
-#Initialize the starting values for the color box display
+# Initialize the starting values for the color box display
 red_value = "00"
 green_value = "00"
 blue_value = "00"
 
-#Run the root window's main loop
+# Run the root window's main loop
 root.mainloop()
--- Simple_Calculator/main.py
+++ Simple_Calculator/main.py
@@ -1,27 +1,28 @@
-#Simple Calculator
+# Simple Calculator
 import tkinter
 from tkinter import RIGHT, END, DISABLED, NORMAL, PhotoImage
 
-#Define window
+# Define window
 root = tkinter.Tk()
 root.title('Calculator')
 root.geometry('350x400')
-root.resizable(0,0)
+root.resizable(0, 0)
 
-#Define colors and fonts
+# Define colors and fonts
 dark_green = '#93af22'
 light_green = '#acc253'
 white_green = '#edefe0'
 button_font = ('Arial', 18)
 display_font = ('Arial', 30)
 
-#Define functions
+
+# Define functions
 def submit_number(number):
     """Add a number or decimal to the display"""
-    #Insert the number or decimal pressed to the end of the display
+    # Insert the number or decimal pressed to the end of the display
     display.insert(END, number)
 
-    #If decimal was pressed, disable the decimal button so it cannot be pressed twice
+    # If decimal was pressed, disable the decimal button so it cannot be pressed twice
     if "." in display.get():
         decimal_button.config(state=DISABLED)
 
@@ -31,14 +32,14 @@ def operate(operator):
     global first_number
     global operation
 
-    #Get the operator pressed and the current value of the display.  This is the first number in the calculation
+    # Get the operator pressed and the current value of the display.  This is the first number in the calculation
     operation = operator
     first_number = display.get()
 
-    #Delete the value (first number) from entry display
+    # Delete the value (first number) from entry display
     display.delete(0, END)
 
-    #Dispable all operator buttons until equal or clear is pressed
+    # Display all operator buttons until equal or clear is pressed
     add_button.config(state=DISABLED)
     subtract_button.config(state=DISABLED)
     multiply_button.config(state=DISABLED)
@@ -47,13 +48,13 @@ def operate(operator):
     inverse_button.config(state=DISABLED)
     square_button.config(state=DISABLED)
 
-    #Return decimal button to normal state
+    # Return decimal button to normal state
     decimal_button.config(state=NORMAL)
 
 
 def equal():
     """Run the stored operation for two number."""
-    #Perform the desired mathematics
+    # Perform the desired mathematics
     if operation == 'add':
         value = float(first_number) + float(display.get())
     elif operation == 'subtract':
@@ -68,16 +69,16 @@ def equal():
     elif operation == 'exponent':
         value = float(first_number) ** float(display.get())
 
-    #Remove the curent value of the display and update it with the answer
+    # Remove the curent value of the display and update it with the answer
     display.delete(0, END)
     display.insert(0, value)
 
-    #Return buttons to normal states
+    # Return buttons to normal states
     enable_buttons()
 
 
 def enable_buttons():
-    """Enabel all butonns on the calculator"""
+    """Enable all buttons on the calculator"""
     decimal_button.config(state=NORMAL)
     add_button.config(state=NORMAL)
     subtract_button.config(state=NORMAL)
@@ -92,106 +93,121 @@ def clear():
     """Clear the display"""
     display.delete(0, END)
 
-    #Return buttons to normal state
+    # Return buttons to normal state
     enable_buttons()
 
 
 def inverse():
     """Calculate the inverse of a given number."""
-    #Do not allow for 1/0
+    # Do not allow for 1/0
     if display.get() == '0':
         value = 'ERROR'
     else:
-        value = 1/float(display.get())
+        value = 1 / float(display.get())
 
-    #Remove the current value in the display and update it with the answer
+    # Remove the current value in the display and update it with the answer
     display.delete(0, END)
     display.insert(0, value)
 
 
 def square():
     """Calculate the square of a given number."""
-    value = float(display.get())**2
+    value = float(display.get()) ** 2
 
-    #Remove the current value in the display and update it with the answer
+    # Remove the current value in the display and update it with the answer
     display.delete(0, END)
     display.insert(0, value)
 
 
 def negate():
     """Negate a given number."""
-    value = -1*float(display.get()) 
+    value = -1 * float(display.get())
 
-    #Remove the current value in the display and update it with the answer
+    # Remove the current value in the display and update it with the answer
     display.delete(0, END)
     display.insert(0, value)
-    
-       
-#GUI Layout
-#Define frames
+
+
+# GUI Layout
+# Define frames
 display_frame = tkinter.LabelFrame(root)
 button_frame = tkinter.LabelFrame(root)
-display_frame.pack(padx=2, pady=(5,20))
+display_frame.pack(padx=2, pady=(5, 20))
 button_frame.pack(padx=2, pady=5)
 
-#Layout for the display frame
+# Layout for the display frame
 display = tkinter.Entry(display_frame, width=50, font=display_font, bg=white_green, borderwidth=5, justify=RIGHT)
 display.pack(padx=5, pady=5)
 
-#Layout for the button frame
+# Layout for the button frame
 clear_button = tkinter.Button(button_frame, text="Clear", font=button_font, bg=dark_green, command=clear)
 quit_button = tkinter.Button(button_frame, text="Quit", font=button_font, bg=dark_green, command=root.destroy)
 
 inverse_button = tkinter.Button(button_frame, text='1/x', font=button_font, bg=light_green, command=inverse)
 square_button = tkinter.Button(button_frame, text='x^2', font=button_font, bg=light_green, command=square)
-exponent_button = tkinter.Button(button_frame, text='x^n', font=button_font, bg=light_green, command=lambda:operate('exponent'))
-divide_button = tkinter.Button(button_frame, text=' / ', font=button_font, bg=light_green, command=lambda:operate('divide'))
-multiply_button = tkinter.Button(button_frame, text='*', font=button_font, bg=light_green, command=lambda:operate('multiply'))
-subtract_button = tkinter.Button(button_frame, text='-', font=button_font, bg=light_green, command=lambda:operate('subtract'))
-add_button = tkinter.Button(button_frame, text='+', font=button_font, bg=light_green, command=lambda:operate('add'))
+exponent_button = tkinter.Button(button_frame, text='x^n', font=button_font, bg=light_green,
+                                 command=lambda: operate('exponent'))
+divide_button = tkinter.Button(button_frame, text=' / ', font=button_font, bg=light_green,
+                               command=lambda: operate('divide'))
+multiply_button = tkinter.Button(button_frame, text='*', font=button_font, bg=light_green,
+                                 command=lambda: operate('multiply'))
+subtract_button = tkinter.Button(button_frame, text='-', font=button_font, bg=light_green,
+                                 command=lambda: operate('subtract'))
+add_button = tkinter.Button(button_frame, text='+', font=button_font, bg=light_green, command=lambda: operate('add'))
 equal_button = tkinter.Button(button_frame, text='=', font=button_font, bg=dark_green, command=equal)
-decimal_button = tkinter.Button(button_frame, text='.', font=button_font, bg='black', fg='white', command=lambda:submit_number("."))
+decimal_button = tkinter.Button(button_frame, text='.', font=button_font, bg='black', fg='white',
+                                command=lambda: submit_number("."))
 negate_button = tkinter.Button(button_frame, text='+/-', font=button_font, bg='black', fg='white', command=negate)
 
-nine_button = tkinter.Button(button_frame, text='9', font=button_font, bg='black', fg='white', command=lambda:submit_number(9))
-eight_button = tkinter.Button(button_frame, text='8', font=button_font, bg='black', fg='white', command=lambda:submit_number(8))
-seven_button = tkinter.Button(button_frame, text='7', font=button_font, bg='black', fg='white', command=lambda:submit_number(7))
-six_button = tkinter.Button(button_frame, text='6', font=button_font, bg='black', fg='white', command=lambda:submit_number(6))
-five_button = tkinter.Button(button_frame, text='5', font=button_font, bg='black', fg='white', command=lambda:submit_number(5))
-four_button = tkinter.Button(button_frame, text='4', font=button_font, bg='black', fg='white', command=lambda:submit_number(4))
-three_button = tkinter.Button(button_frame, text='3', font=button_font, bg='black', fg='white', command=lambda:submit_number(3))
-two_button = tkinter.Button(button_frame, text='2', font=button_font, bg='black', fg='white', command=lambda:submit_number(2))
-one_button = tkinter.Button(button_frame, text='1', font=button_font, bg='black', fg='white', command=lambda:submit_number(1))
-zero_button = tkinter.Button(button_frame, text='0', font=button_font, bg='black', fg='white', command=lambda:submit_number(0))
+nine_button = tkinter.Button(button_frame, text='9', font=button_font, bg='black', fg='white',
+                             command=lambda: submit_number(9))
+eight_button = tkinter.Button(button_frame, text='8', font=button_font, bg='black', fg='white',
+                              command=lambda: submit_number(8))
+seven_button = tkinter.Button(button_frame, text='7', font=button_font, bg='black', fg='white',
+                              command=lambda: submit_number(7))
+six_button = tkinter.Button(button_frame, text='6', font=button_font, bg='black', fg='white',
+                            command=lambda: submit_number(6))
+five_button = tkinter.Button(button_frame, text='5', font=button_font, bg='black', fg='white',
+                             command=lambda: submit_number(5))
+four_button = tkinter.Button(button_frame, text='4', font=button_font, bg='black', fg='white',
+                             command=lambda: submit_number(4))
+three_button = tkinter.Button(button_frame, text='3', font=button_font, bg='black', fg='white',
+                              command=lambda: submit_number(3))
+two_button = tkinter.Button(button_frame, text='2', font=button_font, bg='black', fg='white',
+                            command=lambda: submit_number(2))
+one_button = tkinter.Button(button_frame, text='1', font=button_font, bg='black', fg='white',
+                            command=lambda: submit_number(1))
+zero_button = tkinter.Button(button_frame, text='0', font=button_font, bg='black', fg='white',
+                             command=lambda: submit_number(0))
 
-#First row
+# First row
 clear_button.grid(row=0, column=0, columnspan=2, pady=1, sticky="WE")
 quit_button.grid(row=0, column=2, columnspan=2, pady=1, sticky="WE")
-#Second row
+# Second row
 inverse_button.grid(row=1, column=0, pady=1, sticky="WE")
 square_button.grid(row=1, column=1, pady=1, sticky="WE")
 exponent_button.grid(row=1, column=2, pady=1, sticky="WE")
 divide_button.grid(row=1, column=3, pady=1, sticky="WE")
-#Third row (Add padding to create the size of the columns)
+# Third row (Add padding to create the size of the columns)
 seven_button.grid(row=2, column=0, pady=1, sticky="WE", ipadx=20)
 eight_button.grid(row=2, column=1, pady=1, sticky="WE", ipadx=20)
 nine_button.grid(row=2, column=2, pady=1, sticky="WE", ipadx=20)
 multiply_button.grid(row=2, column=3, pady=1, sticky="WE", ipadx=20)
-#Fourth row
+# Fourth row
 four_button.grid(row=3, column=0, pady=1, sticky="WE")
 five_button.grid(row=3, column=1, pady=1, sticky="WE")
 six_button.grid(row=3, column=2, pady=1, sticky="WE")
 subtract_button.grid(row=3, column=3, pady=1, sticky="WE")
-#Fifth row
+# Fifth row
 one_button.grid(row=4, column=0, pady=1, sticky="WE")
 two_button.grid(row=4, column=1, pady=1, sticky="WE")
 three_button.grid(row=4, column=2, pady=1, sticky="WE")
 add_button.grid(row=4, column=3, pady=1, sticky="WE")
-#Sixth row
+# Sixth row
 negate_button.grid(row=5, column=0, pady=1, sticky="WE")
 zero_button.grid(row=5, column=1, pady=1, sticky="WE")
 decimal_button.grid(row=5, column=2, pady=1, sticky="WE")
 equal_button.grid(row=5, column=3, pady=1, sticky="WE")
 
-#Run the root window's main loop
+# Run the root window's main loop
 root.mainloop()
